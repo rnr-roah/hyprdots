4d3a125f9763c30a33c15f4c4727862cf3f6d563
rnr-roah <rnrroah@gmail.com>
2026-04-03 18:38:08 -0400

    adding backup to install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 DOTS="/home/roah/git-files/hyprdots"
+BACKUP="$HOME/.config/backup"
+
+echo "Creating backup folder..."
+mkdir -p "$BACKUP"
+
+echo "Backing up existing configs..."
+configs=(hypr waybar swaync swayosd matugen alacritty kitty niri nvim fish nitch fastfetch btop cava rofi gtk-3.0 gtk-4.0)
+
+for config in "${configs[@]}"; do
+    if [ -e "$HOME/.config/$config" ] && [ ! -L "$HOME/.config/$config" ]; then
+        echo "  Backing up $config..."
+        mv "$HOME/.config/$config" "$BACKUP/$config.bak"
+    fi
+done
+
+# Backup wallpapers
+if [ -e "$HOME/Wallpapers" ] && [ ! -L "$HOME/Wallpapers" ]; then
+    echo "  Backing up Wallpapers..."
+    mv "$HOME/Wallpapers" "$BACKUP/Wallpapers.bak"
+fi
 
 echo "Creating symlinks..."
+for config in "${configs[@]}"; do
+    ln -sf "$DOTS/.config/$config" "$HOME/.config/$config"
+done
 
-# Configs
-ln -sf "$DOTS/.config/hypr"       ~/.config/hypr
-ln -sf "$DOTS/.config/waybar"     ~/.config/waybar
-ln -sf "$DOTS/.config/swaync"     ~/.config/swaync
-ln -sf "$DOTS/.config/swayosd"    ~/.config/swayosd
-ln -sf "$DOTS/.config/matugen"    ~/.config/matugen
-ln -sf "$DOTS/.config/alacritty"  ~/.config/alacritty
-ln -sf "$DOTS/.config/kitty"      ~/.config/kitty
-ln -sf "$DOTS/.config/niri"       ~/.config/niri
-ln -sf "$DOTS/.config/nvim"       ~/.config/nvim
-ln -sf "$DOTS/.config/fish"       ~/.config/fish
-ln -sf "$DOTS/.config/nitch"      ~/.config/nitch
-ln -sf "$DOTS/.config/fastfetch"  ~/.config/fastfetch
-ln -sf "$DOTS/.config/btop"       ~/.config/btop
-ln -sf "$DOTS/.config/cava"       ~/.config/cava
-ln -sf "$DOTS/.config/rofi"       ~/.config/rofi
-ln -sf "$DOTS/.config/gtk-3.0"    ~/.config/gtk-3.0
-ln -sf "$DOTS/.config/gtk-4.0"    ~/.config/gtk-4.0
-
-# Wallpapers
-ln -sf "$DOTS/wallpapers"         ~/Wallpapers
+ln -sf "$DOTS/wallpapers" "$HOME/Wallpapers"
 
 echo "Done! All symlinks created."
+echo "Old configs backed up to $BACKUP"
